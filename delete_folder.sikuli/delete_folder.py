dcfdad9345d26211c08b267032347ea7c069b3b4
stevenhsu <mingje1104@gmail.com>
2019-10-18 16:47:28 +0800

    modify get user name

--- delete_folder.sikuli/delete_folder.py
+++ delete_folder.sikuli/delete_folder.py
@@ -4,15 +4,17 @@ r = str(current_user)
 print(r)
 rr = r.split("\\")
 ss = rr[1]
-ss = ss[0:-2]
+ss = ss[0:-1]
 print(ss) 
 path_user = ss
 dd = "rd /s/q C:\\Users\\" + path_user + "\\@Qsync_test\\"
 print(dd)
+
 try:
     os.system(dd)
 except:
     pass
+
 """
 os.system('"rd /s/q C:\Users\User\@Qsync_test"')
 """
