9cd3a382406900fea0349ee5daca1544902c833b
stevenhsu <mingje1104@gmail.com>
2019-10-23 14:15:34 +0800

    modify copy_folder for checking folder was existed or not

--- copy_folder.sikuli/copy_folder.py
+++ copy_folder.sikuli/copy_folder.py
@@ -13,6 +13,13 @@ ss = rr[1]
 ss = ss[0:4]
 print(ss) 
 path_user = ss
-dd = "XCOPY D:\\qsynctestdata\\" + week_current + " C:\\Users\\" + path_user + "\\@Qsync_test\\" + week_current + "\ /I"
+copy_path = "dir C:\\Users\\" + path_user + "\\@Qsync_test\\" + week_current
+print(copy_path)
+dd = "XCOPY C:\\PC1_test_files" + " C:\\Users\\" + path_user + "\\@Qsync_test\\" + week_current + "\ /I /E"
 print(dd)
-os.system(dd)
+print(os.system(copy_path))
+if os.system(copy_path) == 1:
+    os.system(dd)
+    print("copy folder")
+else:
+    print("Folder already existed")
